6c88895cadee38b8972fcd2b455be09199239f46
dongmin132 <m914@naver.com>
2025-10-27 15:27:53 +0900

    feat(domain): Car.move() 메서드 구현

--- src/main/java/racingcar/model/domain/Car.java
+++ src/main/java/racingcar/model/domain/Car.java
@@ -1,11 +1,19 @@
 package racingcar.model.domain;
 
+import racingcar.model.rule.MoveStrategy;
+
 public class Car {
     private final String name;
     private int position = 0;
 
     public Car(String name) {
         this.name = name;
+    }
+
+    public void move(MoveStrategy strategy) {
+        if (strategy.isMovable()) {
+            position++;
+        }
     }
 
     public String getName() {
